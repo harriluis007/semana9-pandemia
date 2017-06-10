7ca988ce00f1612e95dd80170941f3ca4576a50c
HARRY SONY <harri.saldivar.s@hotmail.com>
2017-06-10 13:20:44 -0500

    repor nuevo

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp'),
+concat = require('gulp-concat'),
+uglify = require('gulp-uglify'),
+imagemin = require('gulp-imagemin');
+gulp.task('default', ['demo',"imagenmin"]);
+
+gulp.task('demo',function(){
+	gulp.src('js/source/*.js')
+		.pipe(concat('compilado.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('js/build'))
+});
+
+gulp.task("imagenmin",function(){
+	gulp.src("img/*")
+	.pipe(imagemin())
+	.pipe(gulp.dest("imagen"))
+});
